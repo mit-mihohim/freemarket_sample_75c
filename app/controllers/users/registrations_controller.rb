bc71fda350b0663ed40f5b0019fad27041e593cd
Yusuke-Koiwa <smallrock0109@gmail.com>
2020-05-21 16:44:47 +0900

    new_addressアクション仮定義

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -5,9 +5,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
-  # def new
-  #   super
-  # end
+  def new
+    @user = User.new
+  end
+
+  def new_address
+  end
 
   # POST /resource
   # def create
