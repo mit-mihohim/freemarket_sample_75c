bb6623b0b68a99d2ac68117822bbc42b07a93e26
Yusuke-Koiwa <smallrock0109@gmail.com>
2020-05-21 22:12:45 +0900

    ルーティングを一時変更

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   }
   devise_scope :user do
     get'addresses', to: 'users/registrations#new_address'
-    post 'addresses', to: 'users/registrations#create_address'
+    get 'create_addresses', to: 'users/registrations#create_address' # 機能実装時は、post 'addresses' に書き換える
   end
   
   get 'items/index'
