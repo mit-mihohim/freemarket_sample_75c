0691aaa971fca8a4214bb590b8425cee4cc29a98
kitohkazu <64017216+kitohkazu@users.noreply.github.com>
2020-05-22 12:03:35 +0900

    ルーティング修正
    ・ルーティングの記述に統一されていない箇所があったため修正した

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  get 'items/index'
+
   root 'items#index'
-  resources :items, only: :new
+  resources :items, only: [:new, :index]
 end
