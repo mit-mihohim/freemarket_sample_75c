3a45e3a88970ef32cfe2b9d7b9add086e98f0d8f
Jun-Honda-0326 <60055417+Jun-Honda-0326@users.noreply.github.com>
2020-05-25 11:19:21 +0900

    items_table作成

---
+++ db/migrate/20200525013139_create_items.rb
@@ -0,0 +1,16 @@
+class CreateItems < ActiveRecord::Migration[5.2]
+  def change
+    create_table :items do |t|
+      t.string :name, null:false, index:true 
+      t.text :text, null:false
+      t.string :brand
+      t.integer :status, null:false, default:0
+      t.integer :delivery_charge_bearer, null:false, default:0
+      t.integer :shipping_area, null:false, default:0
+      t.integer :delivery_days, null:false, default:0
+      t.integer :price, null:false
+      t.references :user, null:false, foreign_key: true
+      t.timestamps null:false
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_20_224921) do
+ActiveRecord::Schema.define(version: 2020_05_25_013139) do
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "text", null: false
+    t.string "brand"
+    t.integer "status", default: 0, null: false
+    t.integer "delivery_charge_bearer", default: 0, null: false
+    t.integer "shipping_area", default: 0, null: false
+    t.integer "delivery_days", default: 0, null: false
+    t.integer "price", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_items_on_name"
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -25,4 +41,5 @@ ActiveRecord::Schema.define(version: 2020_05_20_224921) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "items", "users"
 end
