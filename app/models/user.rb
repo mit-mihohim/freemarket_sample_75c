6df03b63b2257cce087b0a660b80853240d192d1
Yusuke-Koiwa <smallrock0109@gmail.com>
2020-05-25 20:31:42 +0900

    fixup! userモデルにアソシエーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,11 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :name, presence: true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   has_one :profile, dependent: :destroy
   has_one :address, dependent: :destroy
   accepts_nested_attributes_for :profile
-
-  validates :name, presence: true
 end
