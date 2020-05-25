513a21b259d9d7e2bc8a73e5a7c03edeb08220c4
kitohkazu <64017216+kitohkazu@users.noreply.github.com>
2020-05-25 14:11:31 +0900

    payment_cardsのモデルとテーブル作成
    ・rails g model, rails db:migrateによりモデルとテーブルを作成した

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_20_224921) do
+ActiveRecord::Schema.define(version: 2020_05_25_023339) do
+
+  create_table "payment_cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "payjp_customer_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_payment_cards_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -25,4 +33,5 @@ ActiveRecord::Schema.define(version: 2020_05_20_224921) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "payment_cards", "users"
 end
