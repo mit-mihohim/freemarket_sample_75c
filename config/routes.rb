a2a03db277524c5a3895876967007f688d533650
Yusuke-Koiwa <smallrock0109@gmail.com>
2020-05-25 16:13:02 +0900

    create_addressアクションのルーティング変更

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   }
   devise_scope :user do
     get'addresses', to: 'users/registrations#new_address'
-    get 'create_addresses', to: 'users/registrations#create_address' # 機能実装時は、post 'addresses' に書き換える
+    post 'addresses', to: 'users/registrations#create_address'
   end
   
   root 'items#index'
