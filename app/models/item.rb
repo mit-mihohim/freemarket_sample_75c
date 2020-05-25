5ba1298116fd3e20d64300fd781e6ba3c6ceb607
Jun-Honda-0326 <60055417+Jun-Honda-0326@users.noreply.github.com>
2020-05-25 14:25:01 +0900

    enum定義（配送方法、日数）

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,5 +2,9 @@ class Item < ApplicationRecord
   belongs_to :user
   belongs_to :category
   has_many :item_images, dependent: :destroy
-  accepts_nested_attributes_for :item_images
+  validates :delivery_days, presence: true
+  enum delivery_days: {"選択して下さい": 0, "1〜2日で発送":1, "2〜3日で発送":2, "4〜7日で発送":3 }, _prefix: true
+  enum delivery_charge_bearers: {"選択して下さい": 0, "送料込み（出品者負担）":1, "送料込み（購入者負担）":2}, _prefix: true
+ 
+
 end
