596487d07423841f7fcc12238644fc71898642a0
kitohkazu <64017216+kitohkazu@users.noreply.github.com>
2020-05-26 08:35:39 +0900

    payment_cardsのコントローラーの修正
    ・

--- app/controllers/payment_cards_controller.rb
+++ app/controllers/payment_cards_controller.rb
@@ -1,5 +1,32 @@
 class PaymentCardsController < ApplicationController
-  def index
-    Payjp.api_key = Rails.application.credentials.payjp[:secret_key]
+  #PAYJPとやり取りするために、payjpをロード
+  require "payjp"
+
+
+  def new
+    # cardがすでに登録済みの場合、indexのページに戻します。
+    @card = PaymentCard.where(user_id: current_user.id).first
+
   end
+
+  def create
+    # credentials.yml.encに記載したAPI秘密鍵を呼び出してpay.jpと通信
+    Payjp.api_key = Rails.application.credentials.payjp[:payjp][:secret_key]
+
+    # フォームの値を引数に顧客を作成。
+    customer = Payjp::Customer.create(
+      email: current_user.email, #email:は不要？
+      card: params["payjp_token"],
+      metadata: {user_id: current_user.id} #最悪なくてもOK！
+    )
+
+    # トークン化した情報を自アプリのPayment_cardsテーブルに登録
+    @card = PaymentCard.new(user_id: current_user.id, payjp_customer_id: customer.id)
+    if @card.save
+      #もしcreateビューを作成しない場合はredirect_toなどで表示ビューを指定
+    else
+      redirect_to action: "create"
+    end
+  end
+
 end
