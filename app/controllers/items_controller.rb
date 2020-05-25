12788920cdc61be828ba8785b0f84e05830aebb6
Jun-Honda-0326 <60055417+Jun-Honda-0326@users.noreply.github.com>
2020-05-25 12:18:01 +0900

    item_imagesモデルのネスト設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,6 +4,7 @@ class ItemsController < ApplicationController
 
   def new 
     @item = Item.new
+    @item.item_images.build
   end
 
   def create
@@ -13,7 +14,7 @@ class ItemsController < ApplicationController
 
   private
   def item_params
-    params.require(:item).permit(:name, :text, :brand, :status, :delivery_charge_bearer, :shipping_area, :delivery_days, :price)
+    params.require(:item).permit(:name, :text, :brand, :status, :delivery_charge_bearer, :shipping_area, :delivery_days, :price, item_images_attribute:[:src])
   end
 
   def buy
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :user
   belongs_to :category
-  has_many :items_images, dependent: :destroy
+  has_many :item_images, dependent: :destroy
+  accepts_nested_attributes_for :item_images
 end
