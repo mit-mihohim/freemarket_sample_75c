56a91dff5c837468ce111630bae672d81091cefc
mit-mihohim <mihohim.jp@gmail.com>
2020-05-25 19:10:15 +0900

    ancestryカラムを追加
     interactive rebase in progress; onto 13696b0
     Last commands done (12 commands done):
        pick e037f12 カテゴリテーブル作成
        pick 7b31ba0 ancestryカラムを追加
     Next commands to do (11 remaining commands):
        pick 0de1db3 カテゴリ、リレーション追記
        pick ac93b04 gemfile_update
     You are currently rebasing branch 'カテゴリ機能作成' on '13696b0'.
    
     Changes to be committed:
    	new file:   db/migrate/20200525100607_add_ancestry_to_categories.rb
    	modified:   db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+
 ActiveRecord::Schema.define(version: 2020_05_25_115912) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -65,7 +66,7 @@ ActiveRecord::Schema.define(version: 2020_05_25_115912) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_profiles_on_user_id"
-
+  end
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
