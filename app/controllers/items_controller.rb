7d8f4738739c761143744fbfb40c8f7fb7eb7931
Jun-Honda-0326 <60055417+Jun-Honda-0326@users.noreply.github.com>
2020-05-25 11:27:55 +0900

    items_imagesテーブル作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,8 +2,8 @@ class ItemsController < ApplicationController
   def index
   end
 
-  def new
-    
+  def new 
+    @item = Item.new
   end
 
   def buy
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_25_013139) do
+ActiveRecord::Schema.define(version: 2020_05_25_022602) do
+
+  create_table "item_images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "src", null: false
+    t.bigint "item_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_item_images_on_item_id"
+  end
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -41,5 +49,6 @@ ActiveRecord::Schema.define(version: 2020_05_25_013139) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "item_images", "items"
   add_foreign_key "items", "users"
 end
