0f978d53895ff52ebaa646a255b3d55d4765e193
Yusuke-Koiwa <smallrock0109@gmail.com>
2020-05-25 18:53:40 +0900

    addressモデルにバリデーション追加

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,5 +1,16 @@
 class Address < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :user
+  belongs_to :user, optional: true
   belongs_to_active_hash :prefecture
+
+  with_options presence: true do
+    validates :last_name
+    validates :first_name
+    validates :last_name_kana
+    validates :first_name_kana
+    validates :post_number
+    validates :prefecture_id
+    validates :city
+    validates :house_number
+  end
 end
