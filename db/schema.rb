554bba80a927ecfee39a6366cc156c48d3af2b7e
mit-mihohim <mihohim.jp@gmail.com>
2020-05-25 19:10:15 +0900

    ancestryカラムを追加
     Please enter the commit message for your changes. Lines starting
     with '#' will be ignored, and an empty message aborts the commit.
    
     interactive rebase in progress; onto 34fb168
     Last commands done (4 commands done):
        pick e037f12 カテゴリテーブル作成
        pick 7b31ba0 ancestryカラムを追加
     Next commands to do (4 remaining commands):
        pick 0de1db3 カテゴリ、リレーション追記
        pick ac93b04 gemfile_update
     You are currently rebasing branch 'カテゴリ機能作成' on '34fb168'.
    
     Changes to be committed:
    	new file:   db/migrate/20200525100607_add_ancestry_to_categories.rb
    	modified:   db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_25_064937) do
+ActiveRecord::Schema.define(version: 2020_05_25_100607) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "last_name", null: false
@@ -39,6 +39,12 @@ ActiveRecord::Schema.define(version: 2020_05_25_064937) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_profiles_on_user_id"
+
+
+  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "ancestry"
+    t.index ["ancestry"], name: "index_categories_on_ancestry"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
