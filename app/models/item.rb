844fe8cd33324fa5c2105c4070d9c8fee152944c
Jun-Honda-0326 <60055417+Jun-Honda-0326@users.noreply.github.com>
2020-05-26 12:41:30 +0900

    enum日本語化

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,6 +9,6 @@ class Item < ApplicationRecord
   validates :status_id, :delivery_charge_bearer_id, :shipping_area, :delivery_days_id, numericality: { other_than: 0}
   enum delivery_days: {choose:0, One_Two:1, Two_Three: 2, Three_Four: 3, Four_Seven:4 }, _prefix: true
   enum delivery_charge_bearer: {choose: 0, myself:1, opponent:2 } ,_prefix: true
-  enum status: {choose:0, unused:1, nearly_unused:2, not_injured:3, bit_inujred:4, injured:5, bad:6} ,_prefix: true
+  enum status: {choose:0, unused:1, nearly_unused:2, not_injured:3, bit_injured:4, injured:5, bad:6} ,_prefix: true
 
 end
