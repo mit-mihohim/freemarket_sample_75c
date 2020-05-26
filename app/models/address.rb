9138ce683ba55facf1d966920f2f77100134e868
Yusuke-Koiwa <smallrock0109@gmail.com>
2020-05-26 11:04:27 +0900

    fixup! addressモデルにバリデーション追加

--- app/models/address.rb
+++ app/models/address.rb
@@ -3,14 +3,14 @@ class Address < ApplicationRecord
   belongs_to :user, optional: true
   belongs_to_active_hash :prefecture
 
-  with_options presence: true do
-    validates :last_name
-    validates :first_name
-    validates :last_name_kana
-    validates :first_name_kana
-    validates :post_number
-    validates :prefecture_id
-    validates :city
-    validates :house_number
-  end
+  FULL_WIDTH_REGEX = /\A[ぁ-んァ-ン一-龥]+\z/
+  FULL_WIDTH_KANA_REGEX = /\A[ァ-ヶー－]+\z/
+  POST_NUMBER_REGEX = /\A\d{3}[-]\d{4}\z|\A\d{3}[-]\d{2}\z|\A\d{3}\z|\A\d{5}\z|\A\d{7}\z/
+  PHONE_NUMBER_REGEX = /\A\d{10,11}\z/
+  validates :last_name, :first_name, :last_name_kana, :first_name_kana, :post_number, :prefecture_id, :city, :house_number, presence: true
+  validates :last_name, :first_name, format: { with: FULL_WIDTH_REGEX, message: "は全角で入力して下さい" }
+  validates :last_name_kana, :first_name_kana, format: { with: FULL_WIDTH_KANA_REGEX, message: "はカタカナ（全角）で入力して下さい" }
+  validates :post_number, format: { with: POST_NUMBER_REGEX }
+  validates :phone_number, format: { with: PHONE_NUMBER_REGEX }, allow_blank: true
+
 end
