e752539bd52dee55a5f8226da7d66956af41e9c1
Yusuke-Koiwa <smallrock0109@gmail.com>
2020-05-26 11:05:36 +0900

    fixup! profileモデルのバリデーション追加

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,7 +1,7 @@
 class Profile < ApplicationRecord
   belongs_to :user
 
-  FULL_WIDTH_REGEX = /\A[ぁ-んァ-ン一-龥]/
+  FULL_WIDTH_REGEX = /\A[ぁ-んァ-ン一-龥]\z/
   FULL_WIDTH_KANA_REGEX = /\A[ァ-ヶー－]+\z/
   validates :last_name, :first_name, :last_name_kana, :first_name_kana, :birthday, presence: true 
   validates :last_name, :first_name, format: { with: FULL_WIDTH_REGEX, message: "は全角で入力して下さい" }
