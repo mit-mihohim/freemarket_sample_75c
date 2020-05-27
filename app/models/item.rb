97c59de02fae1cccd5f30fd6e95cc6555857d5d1
Jun-Honda-0326 <60055417+Jun-Honda-0326@users.noreply.github.com>
2020-05-27 15:06:57 +0900

    change_file

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,4 +11,8 @@ class Item < ApplicationRecord
   enum delivery_days: {"1日〜2日で発送":1, "2日〜3日で発送": 2, "3日〜4日で発送": 3, "4日〜7日で発送":4 }, _prefix: true
   enum delivery_charge_bearer: {"送料込み（出品者負担）":1, "着払い（購入者負担）":2} ,_prefix: true
   enum status: {"新品、未使用":1, "未使用に近い":2, "目立った傷や汚れなし":3, "やや傷や汚れあり":4, "傷や汚れあり":5, "全体的に状態が悪い":6} ,_prefix: true
+  belongs_to :user
+  belongs_to :category
+  has_many :items_images, dependent: :destroy
+
 end
