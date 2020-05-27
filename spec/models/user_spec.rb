4683a357ac4aa1220d9fe94d9a08c0fe0efebec6
Yusuke-Koiwa <smallrock0109@gmail.com>
2020-05-27 11:23:12 +0900

    fixup! userモデル単体テスト作成

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,16 +9,28 @@ describe User do
       expect(user).to be_valid
     end
 
-    it "nameがない場合は登録できないこと" do
+    it "nameが存在しない場合は登録できないこと" do
       user = build(:user, name: nil)
       user.valid?
       expect(user.errors[:name]).to include("を入力してください")
     end
 
-    it "emailがない場合は登録できないこと" do
+    it "emailが存在しない場合は登録できないこと" do
       user = build(:user, email: nil)
       user.valid?
       expect(user.errors[:email]).to include("を入力してください")
+    end
+
+    it "passwordが存在しない場合は登録できないこと" do
+      user = build(:user, password: nil)
+      user.valid?
+      expect(user.errors[:password]).to include("を入力してください")
+    end
+
+    it "passwordが存在しても、password_confirmationが存在しない場合は登録できないこと" do
+      user = build(:user, password_confirmation: "")
+      user.valid?
+      expect(user.errors[:password_confirmation]).to include("とパスワードの入力が一致しません")
     end
 
     it "重複したemailが存在する場合は登録できないこと" do
@@ -26,18 +38,6 @@ describe User do
       another_user = build(:user, email: user.email)
       another_user.valid?
       expect(another_user.errors[:email]).to include("はすでに存在します")
-    end
-
-    it "passwordがない場合は登録できないこと" do
-      user = build(:user, password: nil)
-      user.valid?
-      expect(user.errors[:password]).to include("を入力してください")
-    end
-
-    it "passwordが存在しても、password_confirmationがない場合は登録できないこと" do
-      user = build(:user, password_confirmation: "")
-      user.valid?
-      expect(user.errors[:password_confirmation]).to include("とパスワードの入力が一致しません")
     end
 
     it "passwordが7文字以上であれば登録できること" do
