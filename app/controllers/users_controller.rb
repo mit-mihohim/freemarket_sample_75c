76e4b9bce558ebb8f238cbe6a526fe8bcd384eff
Yusuke-Koiwa <smallrock0109@gmail.com>
2020-05-28 11:03:01 +0900

    address 編集のアクション追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action :authenticate_user, only: :show
-  before_action :set_profile, except: :show
+  before_action :set_profile, only: [:edit_profile, :update_profile]
+  before_action :set_address, only: [:edit_address, :update_address]
 
   def show
   end
@@ -18,6 +19,19 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit_address
+  end
+
+  def update_address
+    @address.update(address_params)
+    if @address.save
+      flash[:notice] = "変更内容が保存されました"
+      redirect_to edit_address_users_path
+    else
+      render :edit_address
+    end
+  end
+
   private
 
   def authenticate_user
@@ -32,6 +46,14 @@ class UsersController < ApplicationController
 
   def profile_params
     params.require(:profile).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :birthday)
+  end
+
+  def set_address
+    @address = current_user.address
+  end
+
+  def address_params
+    params.require(:address).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :post_number, :prefecture_id, :city, :house_number, :building_name, :phone_number)
   end
   
 end
