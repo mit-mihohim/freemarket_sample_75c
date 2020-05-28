722bf6c16234afb742b2d627845646833963d32c
Yusuke-Koiwa <smallrock0109@gmail.com>
2020-05-28 10:45:47 +0900

    address 編集のルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -13,8 +13,8 @@ Rails.application.routes.draw do
   get 'items/buy',  to: 'items#buy'
   resources :users, only: :show do
     collection do
-      get 'edit_profile'
-      patch 'update_profile'
+      get 'edit_profile', 'edit_address'
+      patch 'update_profile', 'update_address'
     end
   end
 
