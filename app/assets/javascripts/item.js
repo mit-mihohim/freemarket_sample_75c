c0f60f39b91310181841ebab877e77f620838648
Jun-Honda-0326 <60055417+Jun-Honda-0326@users.noreply.github.com>
2020-05-29 09:19:09 +0900

    複数枚投稿実装（JS）

--- app/assets/javascripts/item.js
+++ app/assets/javascripts/item.js
@@ -1,7 +1,7 @@
 $(document).on('turbolinks:load', ()=> {
   // 画像用のinputを生成する関数
-  let buildFileField = (index)=> {
-    let html = `<div data-index="${index}" class="js-file_group">
+  const buildFileField = (index)=> {
+    const html = `<div data-index="${index}" class="js-file_group">
                     <input class="js-file" type="file"
                     name="item[item_images_attributes][${index}][src]"
                     id="item_images_attributes_${index}_src"><br>
@@ -13,18 +13,18 @@ $(document).on('turbolinks:load', ()=> {
   // file_fieldのnameに動的なindexをつける為の配列
   let fileIndex = [1,2,3,4,5,6,7,8,9,10];
   
-  $('.form-image__box').on('change', '.js-file', function(e) {
+  $('#image-box').on('change', '.js-file', function(e) {
     // fileIndexの先頭の数字を使ってinputを作る
-    $('.form-image__box').append(buildFileField(fileIndex[0]));
+    $('#image-box').append(buildFileField(fileIndex[0]));
     fileIndex.shift();
     // 末尾の数に1足した数を追加する
     fileIndex.push(fileIndex[fileIndex.length - 1] + 1)
   });
     
     //削除の処理
-    $('.form-image__box').on('click', '.js-remove', function() {
+    $('#image-box').on('click', '.js-remove', function() {
       $(this).parent().remove();
       // 画像入力欄が0個にならないようにしておく
-      if ($('.js-file').length == 0) $('.form-image__box').append(buildFileField(fileIndex[0]));
+      if ($('.js-file').length == 0) $('#image-box').append(buildFileField(fileIndex[0]));
     });
 });
