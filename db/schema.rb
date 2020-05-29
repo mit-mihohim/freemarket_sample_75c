9075b44f158f4663bea4f3ee79c64608387d711f
mit-mihohim <mihohim.jp@gmail.com>
2020-05-29 10:41:13 +0900

    アイテムテーブルへカテゴリカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -46,17 +46,15 @@ ActiveRecord::Schema.define(version: 2020_05_25_115912) do
     t.integer "shipping_area", default: 0, null: false
     t.integer "delivery_days", default: 0, null: false
     t.integer "price", null: false
+    t.bigint "categories_id", null: false
     t.bigint "buyer_id"
     t.bigint "seller_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["buyer_id"], name: "index_items_on_buyer_id"
+    t.index ["categories_id"], name: "index_items_on_categories_id"
     t.index ["name"], name: "index_items_on_name"
     t.index ["seller_id"], name: "index_items_on_seller_id"
-  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "ancestry"
-    t.index ["ancestry"], name: "index_categories_on_ancestry"
   end
 
   create_table "profiles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -86,6 +84,7 @@ ActiveRecord::Schema.define(version: 2020_05_25_115912) do
 
   add_foreign_key "addresses", "users"
   add_foreign_key "item_images", "items"
+  add_foreign_key "items", "categories", column: "categories_id"
   add_foreign_key "items", "users", column: "buyer_id"
   add_foreign_key "items", "users", column: "seller_id"
   add_foreign_key "profiles", "users"
