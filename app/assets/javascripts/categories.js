e8deb6e67ac89d9a5bf1ba0ebadcec4ec8eddbcd
mit-mihohim <mihohim.jp@gmail.com>
2020-05-30 18:23:58 +0900

    親カテゴリ選択取得
    ・親カテゴリのvalueを取得

--- app/assets/javascripts/categories.js
+++ app/assets/javascripts/categories.js
@@ -1,6 +1,7 @@
 $(function(){
   // 親カテゴリーセレクトボックスの選択を変えたら、イベント発火
   $("#category_select").on("change", function(){
-    
+    // 取得した親カテゴリーのvalueをcategoryに代入
+    var parentCategory = document.getElementById("category_select").value;
   })
 });
