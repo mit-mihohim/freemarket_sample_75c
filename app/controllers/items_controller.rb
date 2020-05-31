3170fb00577319b17fcd3c3dd184e41456a4d434
mit-mihohim <mihohim.jp@gmail.com>
2020-05-31 23:50:44 +0900

    カテゴリをitem_paramsへ追記

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -13,7 +13,7 @@ class ItemsController < ApplicationController
   end
 
 
-  def create  
+  def create
     begin
       @item = Item.new(item_params)
       if @item.save
@@ -32,7 +32,7 @@ class ItemsController < ApplicationController
 
   private
   def item_params
-    params.require(:item).permit(:name, :text, :brand, :status, :delivery_charge_bearer, :shipping_area, :delivery_days, :price, [item_images_attributes: [:src, :_destroy, :id]]).merge(seller_id: current_user.id)
+    params.require(:item).permit(:name, :text, :category_id, :brand, :status, :delivery_charge_bearer, :shipping_area, :delivery_days, :price, [item_images_attributes: [:src, :_destroy, :id]]).merge(seller_id: current_user.id)
   end
   
   def edit
