af15597a888c030e9ad3dd50cc7c6d76e3846ace
mit-mihohim <mihohim.jp@gmail.com>
2020-05-31 11:14:28 +0900

    選択時のみ動作設定
    ・カテゴリ選択に値が入っている場合のみ、ajax発動するよう設定

--- app/assets/javascripts/categories.js
+++ app/assets/javascripts/categories.js
@@ -30,53 +30,59 @@ $(function(){
   $("#category_select").on("change", function(){
     // 取得した親カテゴリーのvalueをcategoryに代入
     var parentCategory = document.getElementById("category_select").value;
-    // ajaxにて、controllerへ送信
-    $.ajax({
-      url: "/categories/children_category",
-      type: "GET",
-      data: {parentCategory: parentCategory},
-      dataType: "json",
-    })
-    .done(function(children){
-      var insertHtml = "";
-      // 子カテゴリoptionを1つづつ作成
-      children.forEach(function(child){
-        insertHtml += appendOption(child);
-      });
-      // 子カテゴリセレクションの表示
-      appendChildrenSelection(insertHtml);
-      // 親カテゴリが変更されると、子・孫カテゴリを取り除く
-      $("#category_select").on("change", function(){
-        $("#children_categories").remove()
-        $("#grandchildren_categories").remove();
+    // parentCategoryが空でない場合のみAjaxを行う
+    if (parentCategory != ""){
+      // ajaxにて、controllerへ送信
+      $.ajax({
+        url: "/categories/children_category",
+        type: "GET",
+        data: {parentCategory: parentCategory},
+        dataType: "json",
       })
-    })
-    .fail(function(){
-      alert("カテゴリー取得に失敗しました");
-    })
+      .done(function(children){
+        var insertHtml = "";
+        // 子カテゴリoptionを1つづつ作成
+        children.forEach(function(child){
+          insertHtml += appendOption(child);
+        });
+        // 子カテゴリセレクションの表示
+        appendChildrenSelection(insertHtml);
+        // 親カテゴリが変更されると、子・孫カテゴリを取り除く
+        $("#category_select").on("change", function(){
+          $("#children_categories").remove()
+          $("#grandchildren_categories").remove();
+        })
+      })
+      .fail(function(){
+        alert("カテゴリー取得に失敗しました");
+      })
+    }
+    
   });
   // 子カテゴリ選択でイベント発火
   $(document).on("change", "#children_categories", function(){
     // 取得した子カテゴリのvalueを代入
     var childCategory = document.getElementById("children_categories").value;
-    $.ajax ({
-      url: "/categories/grandchildren_category",
-      type: "GET",
-      data: {childCategory: childCategory},
-      dataType: "json"
-    })
-    .done(function(grandchildren){
-      var insertHtml = "";
-      grandchildren.forEach(function(grandchild){
-        insertHtml += appendOption(grandchild);
-      });
-      appendGrandchildrenSelection(insertHtml);
-      $(document).on("change", "#children_categories", function(){
-        $("#grandchildren_categories").remove();
+    if (childCategory != ""){
+      $.ajax ({
+        url: "/categories/grandchildren_category",
+        type: "GET",
+        data: {childCategory: childCategory},
+        dataType: "json"
       })
-    })
-    .fail(function(){
-      alert("カテゴリー取得に失敗しました");
-    })
+      .done(function(grandchildren){
+        var insertHtml = "";
+        grandchildren.forEach(function(grandchild){
+          insertHtml += appendOption(grandchild);
+        });
+        appendGrandchildrenSelection(insertHtml);
+        $(document).on("change", "#children_categories", function(){
+          $("#grandchildren_categories").remove();
+        })
+      })
+      .fail(function(){
+        alert("カテゴリー取得に失敗しました");
+      })
+    }
   })
 });
