6f3c50468af0d799a904f99cb0b5cc4f27595734
kitohkazu <64017216+kitohkazu@users.noreply.github.com>
2020-06-01 18:39:46 +0900

    purchasesコントローラーの作成
    ・purchasesコントローラーの作成、及びbuyアクション、payアクションの作成
    ・コントローラー作成に伴うファイルの生成

---
+++ app/controllers/purchases_controller.rb
@@ -0,0 +1,41 @@
+class PurchasesController < ApplicationController
+  require "payjp"
+  before_action :set_card, :set_item
+
+  def buy
+    if @card.blank?
+      redirect_to new_payment_card_path
+    else
+      Payjp.api_key = Rails.application.credentials.payjp[:secret_key]
+      customer = Payjp::Customer.retrieve(@card.payjp_customer_id)
+      # PAY.JPの顧客情報から、デフォルトで使うクレジットカードを取得する。
+      @card_info = customer.cards.retrieve(customer.default_card)
+      @exp_month = @card_info.exp_month.to_s
+      @exp_year = @card_info.exp_year.to_s.slice(2,3)
+      @address = current_user.address
+    end
+  end
+
+  def pay
+    Payjp.api_key = Rails.application.credentials.payjp[:secret_key]
+    Payjp::Charge.create(
+      :amount => @item.price,
+      :customer => @card.payjp_customer_id,
+      :currency => "jpy"
+    )
+    redirect_to root_path, notice: '支払いが完了しました' 
+  end
+
+
+  private
+
+  def set_card
+    #カード情報
+    @card = PaymentCard.where(user_id: current_user).first
+  end
+
+  def set_item
+    #購入品の情報
+    @item = Item.find(params[:item_id])
+  end
+end
