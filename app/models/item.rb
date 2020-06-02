56211d89b862af51d09de405af6d37c97ed63afc
mit-mihohim <mihohim.jp@gmail.com>
2020-06-02 09:43:15 +0900

    商品レコードの呼び出し追記
    ・商品詳細ページより、レコードの次の商品、前の商品への遷移を可能にする為、商品検索メソッドを定義

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,4 +10,12 @@ class Item < ApplicationRecord
   enum delivery_days: {"1日〜2日で発送":1, "2日〜3日で発送": 2, "3日〜4日で発送": 3, "4日〜7日で発送":4 }, _prefix: true
   enum delivery_charge_bearer: {"送料込み（出品者負担）":1, "着払い（購入者負担）":2} ,_prefix: true
   enum status: {"新品、未使用":1, "未使用に近い":2, "目立った傷や汚れなし":3, "やや傷や汚れあり":4, "傷や汚れあり":5, "全体的に状態が悪い":6} ,_prefix: true
+
+  def previous
+    Item.where("id < ?", self.id).order("id DESC").first
+  end
+
+  def next
+    Item.where("id > ?", self.id).order("id ASC").first
+  end
 end
