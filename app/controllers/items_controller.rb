2198f323064a8921ce922b5eab1529b7077cbc9b
mit-mihohim <mihohim.jp@gmail.com>
2020-06-03 18:51:08 +0900

    indexアクション編集
    ・ページネーション追記

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :set_item, only: [:show, :destroy]
 
   def index
-    @items = Item.where(buyer_id: nil).order("created_at DESC").limit(3)
+    @items = Item.where(buyer_id: nil).order("created_at DESC").page(params[:page]).per(9)
   end
 
   def show
