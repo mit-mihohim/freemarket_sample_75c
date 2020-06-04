cc8e33316bcd91a6aa7689207af74506daebab83
Yusuke-Koiwa <smallrock0109@gmail.com>
2020-06-04 14:04:32 +0900

    users_show.js に追加ページ分のメソッド追記

--- app/assets/javascripts/users_show.js
+++ app/assets/javascripts/users_show.js
@@ -4,9 +4,29 @@ $(function(){
     
     switch(path) {
 
+      case "/users/sell_items":
+        $("#mypage").removeClass("selected");
+        $("#sold_items").addClass("selected");
+        break;
+
+      case "/users/bought_items":
+        $("#mypage").removeClass("selected");
+        $("#bought_items").addClass("selected");
+        break;
+
       case "/users/edit_address":
         $("#mypage").removeClass("selected");
         $("#edit_address").addClass("selected");
+        break;
+
+      case "/payment_cards":
+        $("#mypage").removeClass("selected");
+        $("#payment_method").addClass("selected");
+        break;
+
+      case "/payment_cards/new":
+        $("#mypage").removeClass("selected");
+        $("#payment_method").addClass("selected");
         break;
 
       case "/users/edit":
@@ -19,6 +39,6 @@ $(function(){
         $("#edit_profile").addClass("selected");
         break;
     }
-    
+
   }
 });
