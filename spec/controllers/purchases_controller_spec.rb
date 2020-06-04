a59d26f3a43b620077e130dfe91024ba2fe1403d
kitohkazu <64017216+kitohkazu@users.noreply.github.com>
2020-06-04 14:23:52 +0900

    purchasesコントローラーのbuyのテストコード作成

--- spec/controllers/purchases_controller_spec.rb
+++ spec/controllers/purchases_controller_spec.rb
@@ -1,18 +1,23 @@
-require 'rails_helper'
+require 'rails_helper' # rspecの設定の読み込み
 
-describe PurchasesController do
-  describe 'GET #new' do
-    it "new.html.hamlに遷移すること" do
-      get :new
-      expect(response).to render_template :new
+describe PurchasesController, type: :controller do
+  describe 'GET #buy' do
+    it "buy.html.hamlに遷移すること" do # exampleの説明。以下実際に作動するテストコード。
+      get :buy # 擬似的なリクエスト
+      # 「expect」はエクスペクテーション。評価される式のこと。
+      # 「response」はリクエストが行われた後の遷移先のビューの情報を持つインスタンス。
+      # 「A.to B」は「AがBであること」の意味
+      # 「render_template」マッチャはアクションがリクエストされた時に自動的に遷移するビューを返す
+      expect(response).to render_template :buy
     end
 
-    it "@cardに正しい値が入っていること" do
-      get :new
+    it "@card_infoに正しい値が入っていること" do
     end
 
-    it "indexアクションにリダイレクトすること" do
+    it "カード未登録の場合、PaymentCardsコントローラーのnewアクションにリダイレクトすること" do
     end
   end
 
+  describe 'POST #pay' do
+  end
 end
