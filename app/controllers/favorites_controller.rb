f7d9f4806319be1b556ac51644b2e3b928c85a41
Jun-Honda-0326 <60055417+Jun-Honda-0326@users.noreply.github.com>
2020-06-05 12:20:02 +0900

    コントローラーアクション定義

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,14 @@
 class FavoritesController < ApplicationController
+  
+  def create
+    @favorite = Favorite.create(user_id: current_user.id, item_id: params[:item_id])
+    @item = Item.all
+  end
+
+  def destroy
+    @favorite = Favorite.find_by(user_id: current_user.id, item_id: params[:item_id])
+    @favorite.destroy
+    @item = Item.all  
+  end
+
 end
