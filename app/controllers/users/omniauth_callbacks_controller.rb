30af3b77587da0aa21ef1c7c62c40f7bb4ce2fda
mit-mihohim <mihohim.jp@gmail.com>
2020-06-08 12:52:25 +0900

    新規登録時profileフォーム情報追記
    SNS認証時、profileフォーム情報取得する為

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -9,20 +9,17 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     authorization
   end
 
-  def twitter
-    authorization
-  end
-
   
   private
 
   def authorization
     @user = User.from_omniauth(request.env["omniauth.auth"])
+    @user.build_profile
 
     if @user.persisted?
       sign_in_and_redirect @user, event: :authentication
     else
-      render template: 'devise/registrations/new'
+      render 'devise/registrations/new'
     end
   end
 end
