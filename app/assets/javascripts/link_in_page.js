75ac95e1ab123ae3a2012ff901e7da215d26d119
Yusuke-Koiwa <smallrock0109@gmail.com>
2020-06-08 19:16:36 +0900

    fixup! カテゴリー一覧: ページ内リンク作成

--- app/assets/javascripts/link_in_page.js
+++ app/assets/javascripts/link_in_page.js
@@ -2,7 +2,6 @@ $(function(){
   $('a[href^="#"]').click(function(){
     let href = $(this).attr("href");
     let target = $(href == "#" || href == "" ? 'html' : href);
-    console.log(target);
     let position = target.offset().top;
     $("html, body").animate({scrollTop:position}, 500, 'swing');
     return false;
