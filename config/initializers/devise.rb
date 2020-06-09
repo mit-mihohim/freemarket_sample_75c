663c159ad072bf54d12aac6826d6e25795b5ed73
mit-mihohim <mihohim.jp@gmail.com>
2020-06-09 18:22:30 +0900

    credentials.yml利用に変更

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -259,7 +259,8 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  config.omniauth :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"]
+  config.omniauth :google_oauth2, Rails.application.credentials.google_oauth2[:key],
+                                  Rails.application.credentials.google_oauth2[:secret]
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
