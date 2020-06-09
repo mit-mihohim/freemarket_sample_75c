e1e447846ee12e1e39e4644bd49851b8348b3437
Yusuke-Koiwa <smallrock0109@gmail.com>
2020-06-09 11:15:09 +0900

    カテゴリー詳細にパンくず追加

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -49,3 +49,26 @@ end
 crumb :category_index do
   link "カテゴリー一覧", categories_path
 end
+
+crumb :parent_category do |category|
+  category = Category.find(params[:id]).root
+  link "#{category.name}", category_path(category)
+  parent :category_index
+end
+
+crumb :child_category do |category|
+  category = Category.find(params[:id])
+  if category.has_children?
+    link "#{category.name}", category_path(category)
+    parent :parent_category
+  else
+    link "#{category.parent.name}", category_path(category.parent)
+    parent :parent_category
+  end
+end
+
+crumb :grandchild_category do |category|
+  category = Category.find(params[:id])
+  link "#{category.name}", category_path(category)
+  parent :child_category
+end
